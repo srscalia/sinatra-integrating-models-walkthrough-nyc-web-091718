ca603ccecb99621396c13bbe9f85ef53467d5301
srscalia <shelby.scalia@gmail.com>
2018-10-09 08:35:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,15 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require 'pry'
 
 class App < Sinatra::Base
+# which renders the index.erb page
   get '/' do
     erb :index
   end
-
+  #which receives the form data through params and renders the results page.
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
     erb :results
   end
 end
